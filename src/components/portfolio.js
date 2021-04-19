b30fc3235b216798c3d7b605f4f5db82eb07b343
Melanie <mschellberg27@icloud.com>
2021-04-19 13:41:49 -0400

    added group project 3 to the portfolio

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { CardColumns,Card, Button, Img, Body, Title } from 'react-bootstrap';
+import { CardColumns,Card } from 'react-bootstrap';
 import Img1 from '../img/weather.png';
 import Img2 from '../img/workday.png';
 import Img3 from '../img/team-profile.png';
 import Img4 from '../img/note-taker.png';
 import Img5 from '../img/tic-tac.png';
 import Img6 from '../img/password-gen.png';
+import Img7 from '../img/movie-queue.png';
 
 function Portfolio () {
     return (
@@ -66,14 +67,14 @@ function Portfolio () {
     </Card.Body>
   </Card>
   <Card style={{ width: '18rem' }}>
-    <Card.Img variant="top" src={Img6} />
+    <Card.Img variant="top" src={Img7} />
     <Card.Body>
       <Card.Title>Password Generator</Card.Title>
       <Card.Text>
       Description of the project
       </Card.Text>
-      <a href="https://mschellberg.github.io/passgen/">View Project</a><br></br>
-      <a href="https://github.com/mschellberg/mschellberg.github.io/tree/master/passgen">Github</a>
+      <a href="https://glacial-headland-73425.herokuapp.com/">View Project</a><br></br>
+      <a href="https://github.com/mschellberg/my-movie-queue">Github</a>
     </Card.Body>
   </Card>
   
